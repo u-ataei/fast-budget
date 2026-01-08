a40d43f6994c74c5c060de9e800600f2612230ef
Yousef Ataei <u2001ataei@gmail.com>
2026-01-08 17:31:55 +0330

    feat(expenses): update expense by ID

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, HTTPException, Query, status
+from fastapi import FastAPI, HTTPException, Query, status, Body
 from fastapi.responses import JSONResponse
-from typing import Annotated
+from typing import Annotated, Optional
 from contextlib import asynccontextmanager
 from random import randint
 
@@ -71,5 +71,37 @@ def get_expenses(
 
 
 # Update
+@app.put("/expenses")
+def update_expense(
+    expense_id: int = Query(
+        ge=111111,
+        le=999999,
+        description="""The ID of the expense to update.
+            Must be between 111111 and 999999.
+            """,
+    ),
+    description: Optional[str | None] = Body(
+        ..., description="Description of the expense", embed=True
+    ),
+    amount: Optional[float | None] = Body(
+        ..., gt=0, description="Amount of the expense", embed=True
+    ),
+):
+    if expense_id is not None:
+        for expense in expenses:
+            if expense_id == expense["id"]:
+                expense["description"] = description
+                expense["amount"] = amount
+                return JSONResponse(
+                    {
+                        "message": f"Expense with ID {expense_id} updated.",
+                        "data": expense,
+                    },
+                    status_code=status.HTTP_200_OK,
+                )
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Expense not found!"
+        )
+
 
 # Delete
