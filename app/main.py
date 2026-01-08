7c52680baa49b720d047cd319d0daa1535ff0a71
Yousef Ataei <u2001ataei@gmail.com>
2026-01-08 16:55:40 +0330

    feat(expenses): improve retrieval of expense records

--- app/main.py
+++ app/main.py
@@ -31,22 +31,43 @@ def create_expense(
     }
     expenses.append(expense)
     return JSONResponse(
-        content={"detail": "Expense created successfully!", "data": expense},
+        content={"message": "Expense created successfully!", "data": expense},
         status_code=status.HTTP_201_CREATED,
     )
 
 
 # Retrieve
 @app.get("/expenses")
-def get_expenses(expense_id: Annotated[int | None, Query(ge=111111, le=999999)] = None):
+def get_expenses(
+    expense_id: Annotated[
+        int | None,
+        Query(
+            ge=111111,
+            le=999999,
+            description="""The ID of the expense to retrieve.
+            Must be between 111111 and 999999.
+            If not provided, all expenses will be returned.
+            """,
+        ),
+    ] = None,
+):
     if expense_id is not None:
         for expense in expenses:
             if expense_id == expense["id"]:
-                return expense
+                return JSONResponse(
+                    {
+                        "message": f"Expense with ID {expense_id} found.",
+                        "data": expense,
+                    },
+                    status_code=status.HTTP_200_OK,
+                )
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Expense not found!"
         )
-    return expenses
+    return JSONResponse(
+        {"message": "All expenses retrieved successfully.", "data": expenses},
+        status_code=status.HTTP_200_OK,
+    )
 
 
 # Update
